3dff1bdfe91b755e2cbd79bbc5dade1297b25f6d
Anmol Agrawal <anmolagrawal20003@gmail.com>
2024-10-14 23:01:30 +0530

    Initial DB Setup

--- leadsync/settings.py
+++ leadsync/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "leads",
 ]
+
+AUTH_USER_MODEL = "leads.User"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
