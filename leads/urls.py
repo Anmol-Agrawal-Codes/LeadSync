9470967053383ceaacc33a2394f38b6d35135157
anmol154 <anmolagrawal3002@gmail.com>
2024-10-15 12:02:17 +0000

    Lead creation

--- leads/urls.py
+++ leads/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page, name='home'),
+    path('lead_details/<str:pk>/', views.lead_detail, name='lead-details'),
+    path('create_lead/', views.create_lead, name='create-lead')
 ]
--- leads/views.py
+++ leads/views.py
@@ -1,12 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Lead
+from .models import Lead, Agent
+from .forms import LeadCreation
 # Create your views here.
 
 def home_page(request):
-    # return HttpResponse("hello world")
     lead = Lead.objects.all()
     context={
         "leads": lead
     }
-    return render(request, "leads/home_page.html", context)
+    return render(request, 'leads/home_page.html', context)
+
+def lead_detail(request, pk):
+    lead = Lead.objects.get(id=pk)
+    context = {"lead": lead}
+    return render(request, 'leads/lead_details.html', context)
+
+def create_lead(request):
+    form = LeadCreation()
+    if request.method == 'POST':
+        Lead.objects.create(
+            first_name = request.POST.get('first_name'),
+            last_name = request.POST.get('last_name'),
+            age = request.POST.get('age'),
+            agent = Agent.objects.get(id=request.POST.get('agent'))
+        )
+        return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'leads/create_lead.html', context)
