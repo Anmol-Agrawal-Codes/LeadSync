b133aa7069b9d757470dc1e67442e3499f66948d
anmol154 <anmolagrawal3002@gmail.com>
2024-10-15 10:19:01 +0000

    template setup

--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Lead
 # Create your views here.
+
+def home_page(request):
+    # return HttpResponse("hello world")
+    lead = Lead.objects.all()
+    context={
+        "leads": lead
+    }
+    return render(request, "leads/home_page.html", context)
